37a820139e1855cc5a874b6cff699b41f44bdf83
danny <xdannyrobertsx@gmail.com>
2024-12-06 01:51:30 -0600

    ⭐⭐

--- deno/2/main.js
+++ deno/2/main.js
@@ -1,37 +1,45 @@
-const getSortedLists = (file) => {
+const isValidDiff = (arr) => {
+  let res = true;
+  for (let i = 0; i < arr.length; i++) {
+    if (i === arr.length - 1) {
+        continue;
+    };
+    const currentId = arr[i];
+    const nextId = arr[i + 1];
+    const diff = currentId > nextId
+      ? (currentId - nextId)
+      : (nextId - currentId);
+    if (diff > 0 && diff < 4) {
+      continue;
+  }
+    res = false;
+    break;
+  }
+    return res;
+};
+
+const isValidSort = (arrayOfIds) => {
+  const sortedIds = [...arrayOfIds].sort((a, b) => a - b);
+  const reverseSortedIds = [...sortedIds].reverse();
+  const validSort = arrayOfIds.every((id, index) => id === sortedIds[index]) ||
+    arrayOfIds.every((id, index) => id === reverseSortedIds[index]);
+  if (!validSort) return false;
+  return isValidDiff(arrayOfIds);
+};
+
+const isValidLine = (line) => {
+  const ids = line.split(/\s+/).map((string) => +string);
+  return ids.length > 1 && isValidSort(ids);
+};
+
+const handleFile = (file) => {
   const lines = file.split("\n");
-  const [list1, list2] = lines.reduce((acc, line) => {
-    if (!line.length) return acc;
+  const validLines = lines.filter((line) => isValidLine(line));
+  if (!validLines.length) return "No valid lines found!";
 
-    const nums = line.split(/\s+/);
-    acc[0].push(parseInt(nums[0]));
-    acc[1].push(parseInt(nums[1]));
-    return acc;
-  }, [[], []]);
-
-  return [{ ids: list1.sort(), length: list1.length }, {
-    ids: list2.sort(),
-    length: list2.length,
-  }];
-};
-
-const calculateDistances = (arrayOfListObjects) => {
-  const [list1, list2] = arrayOfListObjects;
-  if (list1.length !== list2.length) return "lists do not match!";
-
-  return list1.ids.reduce((acc, id, index) => {
-    const matchingId = list2.ids[index];
-    if (id === matchingId) return acc;
-    else if (id < matchingId) return acc += (matchingId - id);
-    return acc + (id - matchingId);
-  }, 0);
-};
-
-const getTotalDistance = (file) => {
-  const arrayOfListObjects = getSortedLists(file);
-  return calculateDistances(arrayOfListObjects);
+  return validLines;
 };
 
 const input = await Deno.readTextFile("input.txt");
-const totalDistance = getTotalDistance(input);
-console.log(totalDistance);
+const lines = handleFile(input);
+console.log(`There are ${lines.length} valid lines...`)
