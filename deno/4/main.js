84ed0f1e1bac0036d00a47911135c075a9ba7d31
danny <xdannyrobertsx@gmail.com>
2024-12-20 18:32:39 -0600

    4

--- deno/4/main.js
+++ deno/4/main.js
@@ -11,23 +11,36 @@ const getNumberOfOccurancesInMatrix = (word, matrix) => {
   }
 };
 
-export const splitFile = (file, word) => {
+export const splitFile = (file, targetWord) => {
+  const word = targetWord.toLowerCase();
   const lines = file.toLowerCase().split("\n").filter(Boolean);
   const validLineLength =
     lines.every((line) => line.length === lines[0].length) && lines[0].length;
+
   if (
-    !lines || !validLineLength ||
-    (typeof word !== "string" || word.trim().length() < 1)
-  ) return "No matches found!";
+    !lines ||
+    !validLineLength ||
+    typeof word !== "string" ||
+    word.trim().length < 1
+  )
+    return "No matches found!";
 
-  const countObject = lines.reduce((acc, line) => {
-    if (!line.includes(word[0])) return acc;
+  const countObject = lines.reduce(
+    (acc, line, index) => {
+      if (!line.includes(word[0])) return acc;
+      console.log(index)
 
-    let { count, matrix } = acc;
-    matrix.push(line);
-    count += getNumberOfOccurancesInMatrix(word, matrix);
-    return { count, matrix };
-  }, { count: 0, matrix: [] });
+      let { count, matrix } = acc;
+      matrix.push(line);
+      count += getNumberOfOccurancesInMatrix(word, matrix);
+      return { count, matrix };
+    },
+    { count: 0, matrix: [] }
+  );
 
-  return countObject["count"];
+  return countObject.count;
 };
+
+// does row have x
+// send x index and row index
+// find surrounding x coordinates and traverse matrix
--- deno/4/main.test.js
+++ deno/4/main.test.js
@@ -3,13 +3,14 @@ import { splitFile} from "./main.js";
 
 const example = await Deno.readTextFile("example.txt");
 const input = await Deno.readTextFile("input.txt");
+const targetWord = 'xmas'
 
 Deno.test("get correct output given example", () => {
-  const totalDistance = splitFile(example);
+  const totalDistance = splitFile(example, targetWord);
   expect(totalDistance).toBe(18);
 });
 
 Deno.test("get correct output given input", () => {
-  const totalDistance = splitFile(input);
+  const totalDistance = splitFile(input, targetWord);
   expect(totalDistance).toBe(18);
 });
